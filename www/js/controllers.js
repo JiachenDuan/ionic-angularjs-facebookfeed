7f9137227b9021c6cf61d3b739bbddf737b30516
JiachenDuan <jiachen.duan@gmail.com>
2014-12-09 22:24:23 -0800

    1. add ion-infinite-scroll to loazy load list item

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,15 +3,39 @@ angular.module('starter.controllers', [])
     .controller('DashCtrl', function ($scope) {
     })
 
-    .controller('DiscoveryCtrl', function ($scope, Discovery,$http) {
-        $scope.feeds = Discovery.all();
+    .controller('DiscoveryCtrl', function ($scope, Discovery,$http,$ionicLoading) {
+        $scope.feeds = [];
+            $scope.loadedFeeds = Discovery.all();
+        $scope.noMoreItemsAvailable = false;
+        $scope.loadMore = function() {
+            var numOffeeds=$scope.feeds.length;
+
+                $scope.feeds.push($scope.loadedFeeds[numOffeeds]);
+
+            if($scope.feeds.length == 100){
+                $scope.noMoreItemsAvailable = true;
+            }
+
+            $scope.$broadcast('scroll.infiniteScrollComplete');
+
+        }
+        $scope.showloading = function() {
+            $ionicLoading.show({
+                template: 'Loading...'
+            });
+        };
+        $scope.hideloading = function(){
+            $ionicLoading.hide();
+        };
         $scope.doRefresh = function() {
+         //  $scope.showloading()
             $http.get('/new-items')
                 .success(function(newItems) {
                     $scope.feeds = newItems;
                 })
                 .finally(function() {
                     // Stop the ion-refresher from spinning
+                   // $scope.hideloading()
                     $scope.$broadcast('scroll.refreshComplete');
                 });
         };
